c685e4dc28f52da5e6b5495564b417c346fcd7e9
Alexandre Mély <alexandre.mely@gmail.com>
2017-03-11 22:12:10 +0100

    display improvement

--- functions_linux.sh
+++ functions_linux.sh
@@ -7,9 +7,13 @@ pg_face_init () {
 pg_face_show () {
     local previous_face_pid="$(pgrep gifview)"
     gifview --display :$pg_face_display_num --title "Jarvis" --animate --no-interactive "$1" & # opens new window on top
-    # add sleep?
-    [ -n "$previous_face_pid" ] && kill $previous_face_pid # closes previous window behind
-    # add wait $previous_face_pid 2>/dev/null to suppress Terminated messages
+    if [ -n "$previous_face_pid" ]; then
+        (
+            sleep 0.5 # because above (new) window takes time to open before below (previous) is closed
+            kill $previous_face_pid # closes previous window behind
+            #wait $previous_face_pid 2>/dev/null # to suppress Terminated messages
+        ) & # sleep not blocking
+    fi
 }
 
 pg_face_exit () {
