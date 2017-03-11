ec51f663234b53d2f649d1c8c6265316875bcdb6
Alexandre Mély <alexandre.mely@gmail.com>
2017-03-11 20:55:50 +0100

    bug fix

--- functions_linux.sh
+++ functions_linux.sh
@@ -8,9 +8,9 @@ pg_face_init () {
 }
 
 pg_face_show () {
-    gifview --display 0 --title "Jarvis" --animate --no-interactive "$1" & # opens new window on top
+    gifview --display :0 --title "Jarvis" --animate --no-interactive "$1" & # opens new window on top
     pv_face_pid_current=$! # gets new window pid
-    [ "$pv_face_pid_previous" != "false" ] && kill $pv_face_pid_previous # closes previous window behind
+    [ "$pv_face_pid_previous" != "false" ] && kill $pv_face_pid_previous 2>/dev/null # closes previous window behind
     pv_face_pid_previous=$pv_face_pid_current # save new window pid to close later
 }
 
