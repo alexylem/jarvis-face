89b08c7a9b743be85bf7cfcb8bf7a6e1418ff896
Alexandre Mély <alexandre.mely@gmail.com>
2017-03-11 22:04:00 +0100

    bugfix

--- functions_linux.sh
+++ functions_linux.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
-pv_face_previous_pid=false
-
 pg_face_init () {
     : # do nothing / function definition needed for osx
 }
 
 pg_face_show () {
+    local previous_face_pid="$(pgrep gifview)"
     gifview --display :$pg_face_display_num --title "Jarvis" --animate --no-interactive "$1" & # opens new window on top
-    local pid=$! # gets new window pid
-    $verbose && jv_debug "new face pid=$pid"
-    $verbose && jv_debug "previous face to kill=$pv_face_previous_pid"
-    [ "$pv_face_previous_pid" != "false" ] && kill $pv_face_previous_pid # closes previous window behind
-    pv_face_previous_pid=$pid # save new window pid to close later
-    $verbose && jv_debug "next face to kill=$pv_face_previous_pid"
+    # add sleep?
+    [ -n "$previous_face_pid" ] && kill $previous_face_pid # closes previous window behind
+    # add wait $previous_face_pid 2>/dev/null to suppress Terminated messages
 }
 
 pg_face_exit () {
-    killall -q gifview # killall instead of pv_face_pid_previous just in case...
+    killall -q gifview
 }
