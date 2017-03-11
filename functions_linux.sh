8f9ed7d036a6a29b76cead2a7e073fbbfbc4f303
Alexandre Mély <alexandre.mely@gmail.com>
2017-03-11 20:42:35 +0100

    memory improvement

--- functions_linux.sh
+++ functions_linux.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+
+pv_face_pid_previous=false
+pv_face_pid_current=false
+
 pg_face_init () {
-    : # do nothing
+    : # do nothing / function definition needed for osx
 }
 
 pg_face_show () {
-    gifview --animate "$1" &
+    gifview --animate "$1" & # opens new window on top
+    pv_face_pid_current=$! # gets new window pid
+    $pv_face_pid_previous && kill $pv_face_pid_previous # closes previous window behind
+    pv_face_pid_previous=$pv_face_pid_current # save new window pid to close later
 }
 
 pg_face_exit () {
-    killall -q gifview
+    killall -q gifview # killall instead of pv_face_pid_previous just in case...
 }
