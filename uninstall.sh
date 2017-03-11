de787b04d8c9f0b8686713959b21b2961d14e03a
Alexandre Mély <alexandre.mely@gmail.com>
2017-03-11 22:56:34 +0100

    fixed install script

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 # Use only if you need to undo changes on the user system such as removing software
+if [ "$platform" == "linux" ]; then
+    jv_remove gifsicle
+    jv_yesno "Uninstall Pillow?" && sudo pip remove pillow
+fi
