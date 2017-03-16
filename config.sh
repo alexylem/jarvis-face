56dc5c3ec1bc40ca0cbb114ade671ea4468beae8
Alexandre Mély <alexandre.mely@gmail.com>
2017-03-16 23:11:53 +0100

    alexylem/jarvis#366 animation switches in config

--- config.sh
+++ config.sh
@@ -1,4 +1,9 @@
 pg_face_theme="cortana" # folder name in plugins/jarvis-face/faces/
+pg_face_anim_start=true # enable animation when Jarvis is started
+pg_face_anim_listening=true # enable animation when listening for command
+pg_face_anim_speaking=true # enable animation when speaking
+pg_face_anim_done=true # enable happy animation when conversation is over
+pg_face_anim_exit=true # enable animation when Jarvis terminates
 pg_face_size="" # empty for gif size, 640x480 for fixed size (use for fullscreen)
 pg_face_x_offset="+0" # +X: Xpx from left, -X: Xpx from right
 pg_face_y_offset="+0" # +Y: Ypx from top, -Y: Ypx from bottom
