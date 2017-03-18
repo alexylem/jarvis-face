350d2e1123c20172a2fe02257b4f8a6c9fbd2f8c
Alexandre Mély <alexandre.mely@gmail.com>
2017-03-18 21:13:57 +0100

    alexylem/jarvis#483 added libjpeg-dev dependency

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Use only if you need to perform changes on the user system such as installing software
 if [ "$platform" == "linux" ]; then
-    jv_install gifsicle unclutter
+    jv_install gifsicle unclutter libjpeg-dev # https://github.com/alexylem/jarvis/issues/483
     sudo pip install pillow
 fi
