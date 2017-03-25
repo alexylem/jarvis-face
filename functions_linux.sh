7e14564d93101add80daa8dd737d38f5dc59975b
Alexandre Mély <alexandre.mely@gmail.com>
2017-03-25 16:26:34 +0100

    new image detection and progress bar for resizing

--- functions_linux.sh
+++ functions_linux.sh
@@ -2,20 +2,36 @@
 
 pg_face_init () {
     # if face directory not created yet for desired dimension
+    local to_resize=()
+    shopt -s nullglob
+    local images=( $(cd $pg_face_dir/faces/$pg_face_theme; echo */*.gif) )
     if [ ! -d "$pg_face_dir/faces/$pg_face_theme$pg_face_size" ]; then
-        # initialize from copy of original face folder
-        cp -R "$pg_face_dir/faces/$pg_face_theme" "$pg_face_dir/faces/$pg_face_theme$pg_face_size"
-        # resize all gifs to desired dimension
-        printf "Resizing $pg_face_theme/ into $pg_face_theme$pg_face_size/..."
-        gifsicle --batch \
-                 --resize $pg_face_size \
-                 --no-warnings \
-                 "$pg_face_dir/faces/$pg_face_theme$pg_face_size"/*/*.gif &
-        jv_spinner $! || {
-            jv_error "An error has occured while resizing the face images"
-            jv_exit 1
-        }
-        jv_success "Done"
+        c   p -R "$pg_face_dir/faces/$pg_face_theme" "$pg_face_dir/faces/$pg_face_theme$pg_face_size"
+        to_resize=("${images[@]}")
+    else
+        for image in "${images[@]}"; do
+            if [ ! -f "$pg_face_dir/faces/$pg_face_theme$pg_face_size/$image" ]; then
+                cp "$pg_face_dir/faces/$pg_face_theme/$image" "$pg_face_dir/faces/$pg_face_theme$pg_face_size/$image"
+                to_resize+=("$image")
+            fi
+        done
+    fi
+    local nb_to_resize="${#to_resize[@]}"
+    if [ $nb_to_resize -gt 0 ]; then
+        jv_debug "Resizing $pg_face_theme/ into $pg_face_theme$pg_face_size/..."
+        local i=0
+        jv_progressbar $i $nb_to_resize
+        for image in "${to_resize[@]}"; do
+            gifsicle --batch \
+                     --resize $pg_face_size \
+                     --no-warnings \
+                     "$pg_face_dir/faces/$pg_face_theme$pg_face_size/$image" || {
+                jv_error "An error has occured while resizing the face images"
+                jv_exit 1
+            }
+            let i+=1
+            jv_progressbar $i $nb_to_resize
+        done
     fi
     $pg_face_hide_cursor && unclutter -display $pg_face_display_num -root -idle 0.1 & # hide cursor if not used for 0.1 secs
 }
@@ -31,12 +47,12 @@ pg_face_show () {
         (
             sleep 1 # because above (new) window takes time to open before below (previous) is closed
             kill $previous_face_pid 2>/dev/null # closes previous window behind
-            #wait $previous_face_pid 2>/dev/null # to suppress Terminated messages
+            wait $previous_face_pid 2>/dev/null # to suppress Terminated messages
         ) & # sleep not blocking
     fi
 }
 
 pg_face_exit () {
-    killall -q animate
+    killall -q gifview
     $pg_face_hide_cursor && killall -q unclutter # show back cursor if chosen to hide it
 }
